2fa517103f0750db6bba05b8a27a39f586372ebd
AsraBilal <107981178+asrabilal@users.noreply.github.com>
2022-10-29 13:56:58 -0400

    Add files via upload

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+let sequelize;
+
+// when deployed on Heroku
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // localhost
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3305,
+  });
+}
+
+module.exports = sequelize;
